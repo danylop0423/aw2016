652d1b797bf28ee6b4b1fa6e4c94459c9bc748d5
David Labrador <davlabrador@gmail.com>
2016-05-10 16:14:19 +0200

    Added composer autoloading

--- public/index.php
+++ public/index.php
@@ -10,8 +10,6 @@ if (PHP_SAPI == 'cli-server') {
 
 require __DIR__ . '/../vendor/autoload.php';
 
-require __DIR__ . '/../src/autoload.php';
-
 session_start();
 
 // Instantiate the app
--- src/autoload.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-spl_autoload_register(function ($classname) {
-    require __DIR__  . ('/controllers/' . $classname . '.php');
-});
