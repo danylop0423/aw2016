283d07cd9dbc3db309dbc4b8884630606537cd87
David Labrador <davlabrador@gmail.com>
2016-05-10 17:31:30 +0200

    Middleware testing

--- src/routes.php
+++ src/routes.php
@@ -1,5 +1,15 @@
 <?php
 // Routes
+$app->add(function ($request, $response, $next) use ($app) {
+    $args['x'] = '¿Te gusta lo que ves?';
+    // $response->write($args);
+    // var_dump($app->get('renderer')->addAttribute());die;
+    $renderer = $this->get('renderer');
+    $renderer->addAttribute('x', $args['x']);
+    $response = $next($request, $response, $args);
+
+    return $response;
+});
 
 $app->get('/', 'HomeController:home');
 
