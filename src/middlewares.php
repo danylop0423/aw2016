68c331478061b5197dc1e18c66516105b5ecf3c5
David Labrador <davlabrador@gmail.com>
2016-05-22 03:25:12 +0200

    Updated logged user middleware

--- src/middlewares.php
+++ src/middlewares.php
@@ -5,6 +5,8 @@
 $app->add(function ($request, $response, $next) {
     $loggeduser = isset($_SESSION['loggeduser']) ? unserialize(base64_decode($_SESSION['loggeduser'])) : false;
 
+    $request = $request->withAttribute('loggedUser', $loggeduser);
+
     $renderer = $this->get('renderer');
     $renderer->addAttribute('loggedUser', $loggeduser);
     $response = $next($request, $response);
