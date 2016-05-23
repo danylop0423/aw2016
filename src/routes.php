c390dff3f70cc7e99a6b94bba8b6f82c38a7d805
David Labrador <davlabrador@gmail.com>
2016-05-23 22:39:58 +0200

    Added show auction base

--- src/routes.php
+++ src/routes.php
@@ -1,12 +1,18 @@
 <?php
-// Routes
 
+/* HomeController Routes */
 $app->get('/', 'HomeController:homeAction');
 
 $app->any('/login', 'HomeController:loginAction');
 
 $app->get('/logout', 'HomeController:logoutAction');
 
+
+/* UserController Routes */
 $app->any('/nuevo-usuario', 'UserController:createUserAction');
 
 $app->get('/miperfil','UserController:showProfileAction');
+
+
+/* AuctionController Routes */
+$app->get('/subasta/{id}', 'AuctionController:showAuctionAction');
