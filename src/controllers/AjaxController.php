dcb5a9f1cd9fb58296894218cc812d66b9074e27
David Labrador <davlabrador@gmail.com>
2016-05-27 16:26:39 +0200

    Added new ajax methods

--- src/controllers/AjaxController.php
+++ src/controllers/AjaxController.php
@@ -2,13 +2,42 @@
 
 class AjaxController extends AbstractAjaxController
 {
-    public function testAction($request, $response, $args)
+    public function getSubcategoriesAction($request, $response, $args)
     {
-        $array = array(
-            'hola' => 'HOLA',
-            'adios' => 'ADIOS',
-        );
+        $category = $request->getParam('category');
 
-        return $this->renderJSON($response, $array);
+        if ($category) {
+            $categories = $this->db->select(array('subcategoria.nombre'))
+                ->from('subcategoria')
+                ->join('categoria', 'subcategoria.categoria', '=', 'categoria.id', 'INNER')
+                ->where('categoria.nombre', 'like', htmlspecialchars($category))
+                ->execute()
+                ->fetchAll()
+            ;
+
+            return $this->renderJSON($response, $categories);
+        }
+
+        return $response->withStatus(404);
+    }
+
+    public function getFilteredAuctionsAction($request, $response, $args)
+    {
+        $filters = $request->getParam('filters');
+
+        if ($filters) {
+            $auctions = $this->db->select(array('productos.nombre', 'productos.foto', 'productos.caducidad', 'subasta.pujaMin'))
+                ->from('subasta')
+                ->join('productos', 'subasta.producto', '=', 'productos.id', 'INNER')
+                ->join('subcategoria', 'productos.subcategoria', '=', 'subcategoria.id', 'INNER')
+                ->whereMany($filters, 'like')
+                ->execute()
+                ->fetchAll()
+            ;
+
+            return $this->renderJSON($response, $auctions);
+        }
+
+        return $response->withStatus(404);
     }
 }
