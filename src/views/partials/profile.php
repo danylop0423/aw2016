521f8f3cd1a2b62a683800e19a02a6492e555f3e
David Labrador <davlabrador@gmail.com>
2016-05-28 01:30:24 +0200

    Prueba ahora Dany ;-)

--- src/views/partials/profile.php
+++ src/views/partials/profile.php
@@ -1,4 +1,4 @@
-<div id="contenedor">
+<div id="contenedor" class="profile-page">
 		<div id="data">
 		  <div class="row card-panel light-green lighten-5 z-depth-3">
 			<div class="col s12 flow-text">
@@ -6,7 +6,7 @@
 				  <div class="col s6 card-content">
 					  <h2 class="red-text text-darken-4 card-title">Datos Personales</h2>
 					  <ul class="indigo-text text-darken-4" >
-						<li>Name: <span class="blue-text text-darken-6"><?php echo $loggedUser['nombre']?></span></li> 
+						<li>Name: <span class="blue-text text-darken-6"><?php echo $loggedUser['nombre']?></span></li>
 						<li>Apellido: <span class="blue-text text-darken-6"><?php echo $loggedUser['apellido']?></span></li>
 						<li>e-m@il: <span class="blue-text text-darken-6"><?php echo $loggedUser['email']?></span></li>
 						<li>Teléfono (+34): <span class="blue-text text-darken-6"><?php echo $loggedUser['telefono']?></span></li>
@@ -19,45 +19,45 @@
 							<li><h6><i class="fa fa-chevron-right"></i> Población: <span class="blue-text text-darken-6"><?php echo $loggedUser['poblacion']?></span></h6></li>
 						 </ul></div></li>
 						 </div>
-						<div class="col s7 m7 section"> 
+						<div class="col s7 m7 section">
 						<li>Subastas Activas: <span class="blue-text text-darken-6"> 2 </span></li>
 						<li>Pujas Activas: <span class="blue-text text-darken-6"> 2 </span></li>
 					    </div>
 					  </ul>
 				  </div>
-				 
+
 				 <div class="card-image right ">
 					<img  class="profile circle responsive-img" src="<?php echo $loggedUser['foto']?>">
 					<a class="col s8 btn waves-effect waves-light btn-large" href="/editProfile">
-					<i class="fa fa-pencil right"></i>Editar Datos</a>  
-				  </div>				   
+					<i class="fa fa-pencil right"></i>Editar Datos</a>
+				  </div>
 			 </div>
 
-			  
+
 		     </div>
 		  </div>
 		</div>
 	  </div>
-		
-	
-		
-		
+
+
+
+
 	<div id= "derecont ">
-	   
+
 	   <div class="puja_subasta">
-		  
+
 		  <h1 id="mispujas" class="teal-text text-darken-4">Mis Pujas:</h1>
-			   
+
 		  <div class="row">
 			<div class="col s12 m7">
 			  <div class="card z-depth-2">
 				  <div class="card-image left">
-				   <img src="/assets/images/smartw6.png">	  
+				   <img src="/assets/images/smartw6.png">
 				  </div>
 				  <div class="card-content ">
 					  <span class="red-text text-darken-4 card-title">Producto: X</span>
 					  <ul class="indigo-text text-darken-4" >
-						<li>Estado de la Subasta:<span class="blue-text text-darken-6"> Activa</span></li> 
+						<li>Estado de la Subasta:<span class="blue-text text-darken-6"> Activa</span></li>
 						<li>Puja Ganadora: <span class="blue-text text-darken-6"> 3,00 €</span></li>
 						<li>Mi Ultima Puja: <span class="blue-text text-darken-6">1,00 €</span></li>
 						<li>Tiempo Restante: <span class="blue-text text-darken-6"> 00:03:45</span></li>
@@ -67,17 +67,17 @@
 				</div>
 			</div>
 		  </div>
-		  
+
 		  <div class="row">
 			<div class="col s12 m7">
 			  <div class="card z-depth-2">
 				  <div class="card-image left">
-				   <img src="/assets/images/smartw4.png">	  
+				   <img src="/assets/images/smartw4.png">
 				  </div>
 				  <div class="card-content ">
 					  <span class="red-text text-darken-4 card-title">Producto: X</span>
 					  <ul class="indigo-text text-darken-4" >
-						<li>Estado de la Subasta:<span class="blue-text text-darken-6"> Activa</span></li> 
+						<li>Estado de la Subasta:<span class="blue-text text-darken-6"> Activa</span></li>
 						<li>Puja Ganadora: <span class="blue-text text-darken-6"> 3,00 €</span></li>
 						<li>Mi Ultima Puja: <span class="blue-text text-darken-6">1,00 €</span></li>
 						<li>Tiempo Restante: <span class="blue-text text-darken-6"> 00:03:45</span></li>
@@ -87,24 +87,24 @@
 				</div>
 			</div>
 		  </div>
-		  
-	  </div> 		  
-	
+
+	  </div>
+
 
 	  <div class="puja_subasta">
-			
+
 		  <h1 id="missubastas" class="teal-text text-darken-4">Mis Subastas:</h1>
-			   
+
 		  <div class="row">
 			<div class="col s12 m7">
 			  <div class="card z-depth-2">
 				  <div class="card-image left">
-				   <img src="/assets/images/smartw4.png">	  
+				   <img src="/assets/images/smartw4.png">
 				  </div>
 				  <div class="card-content">
 					  <span class="red-text text-darken-4 card-title">Producto: X</span>
 					  <ul class="indigo-text text-darken-4" >
-						<li>Estado de la Subasta:<span class="blue-text text-darken-6"> Activa</span></li> 
+						<li>Estado de la Subasta:<span class="blue-text text-darken-6"> Activa</span></li>
 						<li>Puja Mínima: <span class="blue-text text-darken-6"> 3,00 €</span></li>
 						<li>Cantidad Ganadora: <span class="blue-text text-darken-6">5,00 €</span></li>
 						<li>Tiempo Restante: <span class="blue-text text-darken-6"> 00:03:45</span></li>
@@ -113,19 +113,19 @@
 				  </div>
 				</div>
 			</div>
-		  </div>		  
-	
-			   
+		  </div>
+
+
 		  <div class="row">
 			<div class="col s12 m7">
 			  <div class="card z-depth-2">
 				  <div class="card-image left">
-				   <img src="/assets/images/smartw6.png">	  
+				   <img src="/assets/images/smartw6.png">
 				  </div>
 				  <div class="card-content">
 					  <span class="red-text text-darken-4 card-title">Producto: X</span>
 					  <ul class="indigo-text text-darken-4" >
-						<li>Estado de la Subasta:<span class="blue-text text-darken-6"> Activa</span></li> 
+						<li>Estado de la Subasta:<span class="blue-text text-darken-6"> Activa</span></li>
 						<li>Puja Mínima: <span class="blue-text text-darken-6"> 3,00 €</span></li>
 						<li>Cantidad Ganadora: <span class="blue-text text-darken-6">5,00 €</span></li>
 						<li>Tiempo Restante: <span class="blue-text text-darken-6"> 00:03:45</span></li>
@@ -134,12 +134,12 @@
 				  </div>
 				</div>
 			</div>
-		  </div>		  
-	
+		  </div>
+
 	</div>
-  </div>				
-				
- </div>	
+  </div>
+
+ </div>
  <script>
     $(function() {
         <?php if ($updated): ?>
@@ -147,4 +147,4 @@
         <?php endif ?>
     });
 </script>
- 
+
